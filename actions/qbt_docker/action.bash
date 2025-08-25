d67e09f5e718a4d3e11d1b44e3afb2ecf08cf855
userdocs <16525024+userdocs@users.noreply.github.com>
2025-08-25 14:45:17 +0100

    Update action.bash

--- actions/qbt_docker/action.bash
+++ actions/qbt_docker/action.bash
@@ -193,7 +193,7 @@ fi
 
 # Note: The yml coming through from the action with is new line separated not space separated
 
-printf '%s' "${{ inputs.custom_docker_commands }}"
+printf '%s\n' "[ERROR] 123: ${{ inputs.custom_docker_commands }}"
 
 # Parse the custom docker commands string into an array
 IFS=$'\n' read -r -a inputs_custom_docker_commands_array <<< "$(printf '%s' "$inputs_custom_docker_commands" | tr -d '\r')"
